ceaa1b6e242e93870479cddd2ef7dc5ac099f767
Ismael Oliveira <ismael6991@live.com>
2023-10-07 11:02:32 -0300

    verificando primeiro, segundo digito do CPF e se o cliente é maior de 18 anos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 import ehUmCPF from './valida-cpf.js'
+import maiorDeIdade from './valida-id.js'
+
 const camposDoFormulario = document.querySelectorAll('[required]')
 
 camposDoFormulario.forEach((campo) => {
@@ -8,5 +10,8 @@ camposDoFormulario.forEach((campo) => {
 function verificaCampo(campo) {
   if (campo.name == 'cpf' && campo.value.length >= 11) {
     ehUmCPF(campo)
+  }
+  if (campo.name == 'aniversario' && campo.value != '') {
+    maiorDeIdade(campo)
   }
 }
